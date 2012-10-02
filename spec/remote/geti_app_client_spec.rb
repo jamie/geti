15606d9b7c3f62a7ca5b044014f95104a7a75a53
Jamie Macey <jamie@tracefunc.com>
2012-10-02 09:49:25 -0700

    get first remote application spec passing

--- spec/remote/geti_app_client_spec.rb
+++ spec/remote/geti_app_client_spec.rb
@@ -2,12 +2,13 @@ require 'helper'
 
 describe Geti::AppClient do
   describe '#board_merchant_ach' do
-    it 'with an empty payload' do
+    it 'has a successful response' do
       client = Geti::AppClient.new(test_app_credentials, {})
       response = client.board_merchant_ach({
-        # empty
+        :id => 123456
       })
-      pp response
+      response[:response][:status].must_equal "Pending"
+      response[:response][:message].must_match /CrossRef: 123456/
     end
   end
 end
