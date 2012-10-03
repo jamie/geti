8a83c1931689fd38f3246dd4deb8ad71005e5b2e
Jamie Macey <jamie@tracefunc.com>
2012-10-03 15:08:32 -0700

    parameterized app call

--- spec/remote/geti_app_client_spec.rb
+++ spec/remote/geti_app_client_spec.rb
@@ -5,10 +5,36 @@ describe Geti::AppClient do
     it 'has a successful response' do
       client = Geti::AppClient.new(test_app_credentials, {})
       response = client.board_merchant_ach({
-        :id => 123456
+        :id               => 123456,
+        :name             => "Cogsley's Cogs",
+        :industry         => "Metal_Fabricators",
+        :address          => "123 Main St",
+        :city             => "Vancouver",
+        :state            => "WA",
+        :zip              => "10120",
+        :phone            => "5555551234",
+        :business_type    => "Corporation",
+        :days_in_business => 404,
+
+        :contact_name     => 'George Jetson',
+        :physical_address => "123 Main St",
+        :physical_city    => "Vancouver",
+        :physical_state   => "WA",
+        :physical_zip     => "10120",
+        :physical_phone   => "5555551234",
+
+        :principal_first_name => "Carl",
+        :principal_last_name  => "Cogsley",
+        :principal_title      => 'President',
+        :principal_address    => "123 Main St",
+        :principal_city       => "Vancouver",
+        :principal_state      => "WA",
+        :principal_zip        => "10120",
+        :principal_dob        => "1965-04-28",
+        :principal_ssn        => '111222123',
       })
-      response[:response][:status].must_equal "Pending"
-      response[:response][:message].must_match /CrossRef: 123456/
+      response[:status].must_equal "Pending"
+      response[:message].must_match /CrossRef: 123456/
     end
   end
 end
