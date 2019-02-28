86817d5907dfa947b911f036c5314e1eeda36f93
Jamie Macey <jamie.git@tracefunc.com>
2019-02-27 22:32:43 -0800

    consolidate specs, one file per class

--- spec/geti_app_client_spec.rb
+++ spec/geti_app_client_spec.rb
@@ -220,3 +220,85 @@ describe Geti::AppClient do
 
   end
 end
+
+describe Geti::AppClient, :vcr => true do
+  before :all do
+    @timestamp = Time.now.to_i
+    pending "Code currently suffering bit rot, API changed out from under me."
+  end
+
+  def merchant_params(ts)
+    { :id               => ts,
+      :name             => "Cogsley's Cogs %d" % ts,
+      :industry         => "Metal_Fabricators",
+      :address          => "123 Main St",
+      :city             => "Vancouver",
+      :state            => "WA",
+      :zip              => "10120",
+      :phone            => "5555551234",
+      :business_type    => "Corporation",
+      :days_in_business => 404,
+
+      :contact_name     => 'George Jetson',
+      :physical_address => "123 Main St, Suite B",
+      :physical_city    => "Vancouver",
+      :physical_state   => "WA",
+      :physical_zip     => "10120",
+      :physical_phone   => "5555551234",
+
+      :principal_first_name => "Carl",
+      :principal_last_name  => "Cogsley",
+      :principal_title      => 'President',
+      :principal_address    => "123 Main St",
+      :principal_city       => "Vancouver",
+      :principal_state      => "WA",
+      :principal_zip        => "10120",
+      :principal_dob        => "1965-04-28",
+      :principal_ssn        => '111222123',
+
+      :average_amount       => "4000",
+      :max_amount           => "7600",
+
+      :taxpayer_name => "Carl Cogsley",
+      :taxpayer_id   => "123456789",
+
+      :routing_number => "490000018",
+      :account_number => "123456789",
+
+      :ip => '127.0.0.1',
+    }
+  end
+
+  describe '#board_merchant_ach' do
+    it 'has a successful response' do
+      client = Geti::AppClient.new(test_credentials)
+      response = client.board_merchant_ach(merchant_params(@timestamp))
+      expect(response[:status]).to eq("Approved")
+      expect(response[:app_data][:merchant][:cross_ref_id]).to eq((@timestamp).to_s)
+      expect(response[:message]).to match(/CrossRef: #{@timestamp}/)
+    end
+  end
+
+  describe '#retrieve_merchant_status' do
+    it 'returns data' do
+      client = Geti::AppClient.new(test_credentials)
+      response = client.board_merchant_ach(merchant_params(@timestamp+1))
+
+      response = client.retrieve_merchant_status(response[:app_data][:merchant][:id])
+      expect(response[:status]).to eq("Approved")
+      expect(response[:app_data][:merchant][:cross_ref_id]).to eq((@timestamp+1).to_s)
+      expect(response[:message]).to eq("Merchant Approved")
+    end
+  end
+
+  describe '#upload_supporting_docs' do
+    it 'returns data' do
+      client = Geti::AppClient.new(test_credentials)
+      response = client.board_merchant_ach(merchant_params(@timestamp+2))
+
+      response = client.upload_supporting_docs(response[:app_data][:merchant][:id], File.read('./spec/fixtures/sample.pdf'))
+      expect(response[:status]).to eq("Approved")
+      expect(response[:message]).to match("Document uploaded successfully")
+    end
+  end
+end
--- spec/geti_auth_client_spec.rb
+++ spec/geti_auth_client_spec.rb
@@ -20,3 +20,103 @@ describe Geti::AuthClient do
     end
   end
 end
+
+describe Geti::AuthClient, :vcr => true do
+  def routing_number(type)
+    { :authorization  => 490000018,
+      :decline        => 490000034,
+      :manager_needed => 490000021,
+      :represented    => 490000047
+    }[type]
+  end
+
+  let(:credentials) { test_credentials('auth') }
+
+  describe '#get_terminal_settings' do
+    it 'gets a successful WEB response' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'WEB', :verify => []})
+      terminal_settings = client.get_terminal_settings
+      expect(terminal_settings.terminal_id).to eq("2310")
+    end
+
+    it 'gets a successful PPD response' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'PPD', :verify => []})
+      terminal_settings = client.get_terminal_settings
+      expect(terminal_settings.terminal_id).to eq("1010")
+    end
+
+    it 'gets a restricted WEB response' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'WEB', :verify => [:dl]})
+      terminal_settings = client.get_terminal_settings
+      expect(terminal_settings.terminal_id).to eq("2311")
+    end
+  end
+
+  describe '#validate' do
+    it 'gets a failed WEB response' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'WEB', :verify => []})
+      response = client.validate({})
+      expect(response.validation.result).to eq("Failed")
+      expect(response).to_not be_success
+      expect(response.errors).to include("The 'CHECK_AMOUNT' element has an invalid value according to its data type.")
+    end
+
+    it 'gets a successful WEB response' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'WEB', :verify => []})
+      response = client.validate({
+        :type => :authorize,
+        :amount => 1000,
+        :first_name => 'Bob',
+        :last_name => 'Smith',
+        :account_type => 'Checking',
+        :routing_number => '123456778',
+        :account_number => '1234567890'
+      })
+      expect(response.validation.result).to eq("Passed")
+      expect(response).to be_success
+      expect(response.errors).to be_empty
+    end
+  end
+
+  describe '#process' do
+    it 'gets a failed WEB response' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'WEB', :verify => []})
+      response = client.process({})
+      expect(response.validation.result).to eq("Failed")
+      expect(response).to_not be_success
+      expect(response.errors).to include("The 'CHECK_AMOUNT' element has an invalid value according to its data type.")
+    end
+
+    it 'gets an exception' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'WEB', :verify => []})
+      response = client.process({
+        :type => :authorize,
+        :amount => 1000,
+        :first_name => 'Bob',
+        :last_name => 'Smith',
+        :account_type => 'Checking',
+        :routing_number => '123456778',
+        :account_number => '1234567890'
+      })
+      expect(response).to_not be_success
+      expect(response.exception.message).to_not be_empty
+    end
+
+    it 'gets a successful WEB response' do
+      client = Geti::AuthClient.new(credentials, {:sec_code => 'WEB', :verify => []})
+      response = client.process({
+        :type => :authorize,
+        :amount => 1000,
+        :first_name => 'Bob',
+        :last_name => 'Smith',
+        :account_type => 'Checking',
+        :routing_number => routing_number(:authorization),
+        :account_number => '1234567890'
+      })
+      expect(response.validation.result).to eq("Passed")
+      expect(response).to be_success
+      expect(response.errors).to be_empty
+      expect(response.authorization.message).to eq("APPROVAL")
+    end
+  end
+end
